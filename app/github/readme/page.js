077cc1c9df3a665feca9ddbd27e5726d2f8214d0
Lucky Raj <kumari.lucky.raj.cse.2023@tint.edu.in>
2026-01-30 13:43:16 +0530

    Revert "hello"
    This reverts commit 00046aac720e8690444b884acf3e96bc6fc1f12e.

--- app/github/readme/page.js
+++ app/github/readme/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-//before revert.
+
 export default function GitHubReadme() {
   const [username, setUsername] = useState('');
   const [techInput, setTechInput] = useState('');
